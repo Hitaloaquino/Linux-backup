41f42a03a4e7a8a73d3c955987863220591f082c
hitaloaquino <suportesite2014@gmail.com>
2024-07-06 21:15:37 -0300

    Update backup-completo.sh

--- backup-completo.sh
+++ backup-completo.sh
@@ -5,7 +5,7 @@ destino_backup="/mnt/backup/opt"
 formato_data=$(date "+%d-%m-%Y %H:%M:%S")
 final_arquivo="backup-$formato_data.tar.gz"
 arquivo_log="/var/log/dia-backup.log"
-if tar -czSf "$destino_backup/$final_arquivo" "$origem_backup"; then
+if tar -cvzf "$destino_backup/$final_arquivo" "$origem_backup"; then
    echo "[$formato_data] BACKUP SUCESSO.\n" >> $arquivo_log
 else
    echo "[$formato_data] ERROR BACKUP.\n" >> $arquivo_log
